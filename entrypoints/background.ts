aa42056f07a54b5eb1b3164b2cc4705b2bec8113
DraconDev <dracsharp@gmail.com>
2024-12-11 11:16:44 +0000

    fix bg script

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,27 +1,5 @@
 import { defineBackground } from 'wxt/sandbox';
 
 export default defineBackground(() => {
-    console.log("Background script initialized", { id: browser.runtime.id });
-
-    // Listen for messages from content script
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-        console.log("Background received message:", message);
-        
-        // Handle different message types
-        switch (message.action) {
-            case "START_AUTO_APPLY":
-            case "STOP_AUTO_APPLY":
-            case "GET_JOB_DETAILS":
-            case "skipToLastJob":
-                // Forward message to active tab
-                if (sender.tab?.id) {
-                    chrome.tabs.sendMessage(sender.tab.id, message)
-                        .catch(error => console.error("Error forwarding message:", error));
-                }
-                break;
-        }
-
-        // Always return true if we're using sendResponse asynchronously
-        return true;
-    });
+    console.log("Background script initialized");
 });
