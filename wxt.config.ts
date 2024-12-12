8f99e440e39a12ac378cc8280c3a588412612d8c
DraconDev <dracsharp@gmail.com>
2024-12-12 15:11:11 +0000

    upd host permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
         description: "Automate your LinkedIn job application process",
         version: "1.4.75",
         // This is a development public key, it will generate a consistent ID
-        key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhm3X7qutsrskke84ltbOvV/BgZWLDjFJBovR6vqQZ+DgrXj3K/RqCxHRYABHIYAZWEg4GXq+JqkbBU4L5EUmZf0j2+HMHWZPrZph7/Xs3WUk5jEakL1x0LYr4KDZmQJrxrXBCWsUM1qwGgsHJKp7LW/GXPTEUPMRRngbKZRBKVqpgz+QWkKonI5CCqpKcYJRN0uGRhFyL/0OYg8BL8r2/CTN3WnvBo1YoHwzEhqp9Jt9yCnvWrn9qLRMvIXeZ4CmxQPRoVPrqXt/zPUz9nEGQJyJw7S5nki7B2V+HcwH8QyvB3RvT1BJp5yj9ZKwZ4jEqUgEp/EGyVwEv9r8cwIDAQAB",
         permissions: ["storage", "windows", "tabs"],
-        host_permissions: ["*://*/*"],
+        host_permissions: ["https://*.linkedin.com/*"],
         action: {
             default_popup: "entrypoints/popup/index.html",
+            default_title: "LinkedIn Auto Apply Pro",
         },
     },
     modules: ["@wxt-dev/module-react"],
