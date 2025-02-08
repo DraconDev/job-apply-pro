3a501cf6e97f3311b3a07208e1779eae11652566
DraconDev <dracsharp@gmail.com>
2025-02-08 23:56:01 +0000

    feat: update 1 files (components/FilterButton.tsx) at 23:55

--- components/FilterButton.tsx
+++ components/FilterButton.tsx
@@ -6,10 +6,10 @@ export default function FilterButton() {
   return (
     <button
       onClick={openFilterPage}
-      className="w-full px-4 py-2.5 bg-orange-400 hover:bg-orange-500 text-white rounded-lg text-sm font-medium transition-colors flex items-center justify-center shadow-sm"
+      className="relative w-full px-4 py-2.5 bg-orange-400 hover:bg-orange-500 text-white rounded-lg text-sm font-medium transition-colors flex items-center shadow-sm"
     >
       <svg
-        className="w-4 h-4 mr-2"
+        className="absolute w-4 h-4 left-4"
         fill="none"
         stroke="currentColor"
         viewBox="0 0 24 24"
@@ -21,7 +21,7 @@ export default function FilterButton() {
           d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
         />
       </svg>
-      <span className="text-base">Filter Settings</span>
+      <span className="flex-grow text-base text-center">Filter Settings</span>
     </button>
   );
 }
