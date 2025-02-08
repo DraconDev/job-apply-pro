ac16fb6f2d82953ec1662408fc63c193496bf022
DraconDev <dracsharp@gmail.com>
2025-02-08 23:58:01 +0000

    feat: update 1 files (components/HelpButton.tsx) at 23:57

--- components/HelpButton.tsx
+++ components/HelpButton.tsx
@@ -51,7 +51,10 @@ const HelpButton: React.FC<HelpButtonProps> = ({ className = "" }) => {
                         d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                     />
                 </svg>
-                <span className="flex-grow text-base text-center">Donate ❤️</span>
+                <span className="flex-grow text-base text-center">
+                    <span>Donate</span>
+                    <span className="inline-block ml-1 align-middle">❤️</span>
+                </span>
             </button>
         </div>
     );
